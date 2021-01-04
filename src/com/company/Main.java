0c6f4f97956a56477f26ddf5f75a9d7e5de969c2
LeShawn Edouard <leshawnmedouard@gmail.com>
2021-01-04 12:09:19 -0500

    Created Leap Year method to determine if the given 'year' is a leap year or not.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -168,4 +168,30 @@ public class Main {
                 break;
         }
     }
+
+    // Number of Days In Month Challenge
+    // Write a method 'isLeapYear' with a parameter of type int named 'year'
+    // The parameter needs to be 'greater than or equal to 1' and 'less than
+    // or equal to 9999'.
+    // If the parameter is not in that range return 'false'.
+    // Otherwise, if it is in the valid range, calculate if the year is a leap
+    // year and return 'true' if it is, otherwise return 'false'
+
+    public static boolean isLeapYear(int year) {
+        if((year <= 1) || (year >= 9999)) {
+            return false;
+        }
+        if(year % 4 == 0 && year % 100 != 0) {
+            return false;
+        }
+        return true;
+    }
+
+
+    // Write another method 'getDaysInMonth' with two parameters 'month'
+    // and 'year'.Both of type 'int'.
+    // If parameter month is < 1 or > 12 return -1.
+    // If parameter year is < 1 or > 9999 then return -1.
+    // This method needs to return the number of days in the month. Be careful
+    // about leap years they have 29 days in month 2 (February).
 }
