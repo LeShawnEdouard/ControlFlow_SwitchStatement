762dd9eb0ecdb26fff4d3a956f5bbcf4bc2618cd
LeShawn Edouard <leshawnmedouard@gmail.com>
2021-01-03 19:30:39 -0500

    Day of the week challenge. Created void method with one parameter of type int. Day of the week will print based on matching argument that is past through.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -79,5 +79,47 @@ public class Main {
             default:
                 System.out.println("Not sure");
         }
+
+        printDayOfTheWeek(4);
+    }
+
+
+    // Day of the Week Challenge
+    // Write a method with the name 'printDayOfTheWeek' that has one parameter
+    // of type 'int' and name it 'day'.
+
+    // The method should not return any value (hint: void)
+
+    // Using a switch statement print "Saturday", "Monday",...,"Saturday" if
+    // the int parameter "day" is 0, 1,..., 6 respectively, otherwise it
+    // should print "Invalid day".
+
+    public static void printDayOfTheWeek(int day) {
+        switch(day) {
+
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Day not found");
+        }
     }
 }
