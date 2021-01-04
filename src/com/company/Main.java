564cd0e987f50f6f8f1c58276de2337ea6fb8798
LeShawn Edouard <leshawnmedouard@gmail.com>
2021-01-04 12:17:05 -0500

    Added condition if the given year is 'not' divisible by 400

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -82,6 +82,11 @@ public class Main {
 
         printDayOfTheWeek(4);
 	    printNumberInWord(3);
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+
     }
 
 
@@ -182,6 +187,9 @@ public class Main {
             return false;
         }
         if(year % 4 == 0 && year % 100 != 0) {
+            return false;
+        }
+        if(year % 400 != 0) {
             return false;
         }
         return true;
