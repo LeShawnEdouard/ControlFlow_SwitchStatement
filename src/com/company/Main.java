35b36e3b965943b0bf9836e5b8af3be9bd8fa2ed
LeShawn Edouard <leshawnmedouard@gmail.com>
2021-01-06 10:28:09 -0500

    Created second method with int data types of 'month' and 'year'. Created conditions for year and month. Called previous method to determine leap year. Creating switch case for given month.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -195,11 +195,24 @@ public class Main {
         return true;
     }
 
-
     // Write another method 'getDaysInMonth' with two parameters 'month'
     // and 'year'.Both of type 'int'.
     // If parameter month is < 1 or > 12 return -1.
     // If parameter year is < 1 or > 9999 then return -1.
     // This method needs to return the number of days in the month. Be careful
     // about leap years they have 29 days in month 2 (February).
+
+    public static int getDaysInMonth(int month, int year) {
+        if((month < 1) || (month > 12)) {
+            return -1;
+        }
+        if((year < 1) || (year > 9999)) {
+            return -1;
+        }
+        isLeapYear(year);
+
+        switch(month) {
+
+        }
+    }
 }
